7c666a478cbebbdf918fb97a95f46c551c956e7a
bronzdoc <lsagastume1990@gmail.com>
2016-06-18 23:56:00 -0600

    Handle connections

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
 var (
@@ -18,11 +20,13 @@ func main() {
 	//	"get",
 	//	"cd",
 	//}
-	flag.StringVar(&host, "host", "127.0.0.1:20", "Listening host")
+	flag.StringVar(&host, "host", "127.0.0.1:4000", "Listening host")
 	listener, err := net.Listen("tcp", host)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Starting goftp at", time.Now().Format("15:04:05\n"))
 
 	for {
 		conn, err := listener.Accept()
@@ -36,8 +40,17 @@ func main() {
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
-	_, err := io.WriteString(conn, "Connected to goftp")
+	_, err := io.WriteString(conn, "Connected to goftp\n")
 	if err != nil {
 		return
+	}
+	for {
+		buffer := make([]byte, 512)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		fmt.Printf("%s", buffer)
 	}
 }
