9db6b9f764cfc8373a89ddf3f6c40526afbcf3f5
bronzdoc <lsagastume1990@gmail.com>
2016-06-27 21:14:03 -0600

    Move ftp related stuff to ftp

--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
-	"os/exec"
-	"strings"
 	"time"
 )
 
 var (
 	host string
+	ftp  FTPServer
 )
 
 func main() {
@@ -31,71 +28,6 @@ func main() {
 			log.Fatal(err)
 			continue
 		}
-		go handleConn(conn)
+		go ftp.HandleConn(conn)
 	}
-}
-
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	_, err := io.WriteString(conn, "Connected to goftp\n")
-	if err != nil {
-		return
-	}
-
-	for {
-		buffer := make([]byte, 256)
-		_, err := conn.Read(buffer)
-		if err != nil {
-			log.Print(err)
-			return
-		}
-
-		command, args := getCommandAndArgs(buffer)
-		out, err := handleCommand(command, args)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if out == "quit" {
-			conn.Close()
-		}
-
-		io.WriteString(conn, out)
-	}
-}
-
-func getCommandAndArgs(buffer []byte) (command string, args []string) {
-	message := strings.Fields(string(buffer))
-	command = strings.TrimSpace(message[0])
-	args = message[1:len(message)]
-	return command, args
-}
-
-func handleCommand(command string, args []string) (string, error) {
-	var cmd *exec.Cmd
-
-	switch command {
-	case "LIST":
-		cmd = exec.Command("ls", "-l")
-	case "CWD":
-		cmd = exec.Command("echo", "changed dir")
-		os.Chdir(args[0])
-	case "PWD":
-		cmd = exec.Command("bash", "-c", "pwd")
-	case "QUIT":
-		return "quit", nil
-	case "MKD":
-		cmd = exec.Command("mkdir", args[0])
-	case "DELE":
-		cmd = exec.Command("rm", "-rf", args[0])
-	default:
-		return fmt.Sprintf("Invalid command: %s\n", command), nil
-	}
-
-	out, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	return string(out), nil
 }
